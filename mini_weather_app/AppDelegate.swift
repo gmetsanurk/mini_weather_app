59ef5f96e9d6b72785a6f189a601389ff444dd72
Georgy Metsanurk <gmetsanurk@gmail.com>
2025-03-12 15:54:00 +0300

    Add Info.plist

---
+++ mini_weather_app/AppDelegate.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(
+        _: UIApplication,
+        didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        setupWindow()
+        return true
+    }
+    
+    private func setupWindow() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let homeVC = HomeScreen()
+        window?.rootViewController = homeVC
+        window?.makeKeyAndVisible()
+    }
+}
+
+
