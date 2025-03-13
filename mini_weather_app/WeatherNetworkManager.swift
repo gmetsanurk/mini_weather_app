5717ccf98443b21cfbb164949c98e1e89d33f53d
Georgy Metsanurk <gmetsanurk@gmail.com>
2025-03-13 17:55:33 +0300

    Begin implementing fetchWeather() & request()

--- mini_weather_app/WeatherNetworkManager.swift
+++ mini_weather_app/WeatherNetworkManager.swift
@@ -1,9 +1,27 @@
 import Foundation
 
+enum NetworkError: Error {
+    case invalidURL
+}
+
 class WeatherNetworkManager {
     static let shared = WeatherNetworkManager()
     private let apiKey = "a2b4f69ada2c898ba84feba0043fc80a"
     private let baseUrl = "api.openweathermap.org"
     
     private init() {}
+    
+    func fetchWeather(for city: String, completion: @escaping (Result<WeatherData, Error>) -> Void) {
+        
+        let urlString = "\(baseUrl)?q=\(city)&appid=\(apiKey)&units=metric&lanr=ru"
+        //request(urlString: urlString)
+    }
+    
+    func request<T: Codable>(urlString: String, completion: @escaping (Result<T, Error>) -> Void) {
+        
+        guard let url = URL(string: urlString) else {
+            completion(.failure(NetworkError.invalidURL))
+            return
+        }
+    }
 }
