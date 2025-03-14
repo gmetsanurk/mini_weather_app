9134de2a3f1108a35ac5ff87ff6dcd61e5109c10
Georgy Metsanurk <gmetsanurk@gmail.com>
2025-03-14 21:15:23 +0300

    Work with WeatherViewControllert

--- mini_weather_app/WeatherViewController.swift
+++ mini_weather_app/WeatherViewController.swift
@@ -7,10 +7,29 @@ class WeatherViewController: UIViewController {
     private let refreshButton = UIButton(type: .system)
     
     private let weatherViewModel = WeatherViewModel()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
 
+        weatherViewModel.fetchWeather(for: "Moscow")
+    }
+    
+    
+    
+    private func getWeatherIcon(for description: String) -> String {
+        if description.contains("rain") {
+            return "cloud.rain.fill"
+        } else if description.contains("clear") {
+            return "sun.max.fill"
+        } else {
+            return "cloud.fill"
+        }
+    }
+    
+    private func showError(_ message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
 }
 
